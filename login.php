f4ee8d2f4bfe2574cf6fe56c929f4157ee40e749
Christine Rohacz <crohacz@gmail.com>
2015-10-18 19:24:08 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML> 
+<html> 
+	<head> 
+		<title> SCU Equivalents </title> 
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css"> 
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="/js/script.js"></script>
+	</head> 
+	
+	<body> 
+	<nav class="navbar navbar-default"> 
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">
+					<img id="logo" alt="Brand" src="https://cloud.githubusercontent.com/assets/4735087/10567984/20b70294-75c6-11e5-941f-efffff4745bb.png"> 
+					<a href="login.php"> <button id="login" type="button" class="btn btn-default">Login</button></a>
+				</a>
+			</div>
+	</nav>
+	<div class="container">
+  		<form class="form-signin">
+    		<h2 class="form-signin-heading">Login</h2>
+				<input type="text" class="input-block-level" placeholder="Username">
+				<input type="password" class="input-block-level" placeholder="Password">
+				<input type="submit" class="btn btn-primary">
+		</form>
+	</div>
+	</body> 
+</html> 
