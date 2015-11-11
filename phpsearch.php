498a0fc4cf2dd41503f33cba90ef87c62eb67767
Christine Rohacz <crohacz@gmail.com>
2015-11-11 15:00:10 -0800

    Update phpsearch.php
    updated searching to select ALL

--- phpsearch.php
+++ phpsearch.php
@@ -1,126 +1,113 @@
 <?php
-$con=mysqli_connect("dbserver.engr.scu.edu","crohacz","00000896245","sdb_crohacz");
-// Check connection
+include 'config.php';
+if(isset($_POST['submit'])) {
 
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+        $all = "ALL";
 
-if(isset($_POST['submit'])) { 
-	
-	if(empty($_POST['university']) && empty($_POST['course_name']) && empty($_POST['course_number']) && empty($_POST['scu_equivalent'])) { 
-		$result = mysqli_query($con, "SELECT * FROM courses"); 
-	} 
+        $university = $_POST["university"];
+        $course_name = $_POST["course_name"];
+        $course_number = $_POST["course_number"];
+        $equivalent = $_POST["scu_equivalent"];
 
-	else { 
+        if($university == $all and $course_name == $all and  $course_number == $all and $equivalent == $all) {
+                $result = mysqli_query($con, "SELECT * FROM courses");
+        }
 
-		$university = $_POST["university"];  
-		$course_name = $_POST["course_name"];
-		$course_number = $_POST["course_number"];
-		$equivalent = $_POST["scu_equivalent"];
+        else if($course_name == $all and $course_number == $all and $equivalent == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses  WHERE university='$university'");
+        }
 
-		if(empty($_POST['course_name']) and empty($_POST['course_number']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses  WHERE university='$university'");
-		}
- 
-		else if(empty($_POST['university']) and empty($_POST['course_number']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_name='$course_name'");
-		}
- 
-		else if(empty($_POST['university']) and empty($_POST['course_name']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_number='$course_number'");
-		} 
+        else if($university == $all and $course_number == $all and $equivalent == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_name='$course_name'");
+        }
 
-		else if(empty($_POST['university']) and empty($_POST['course_name']) and empty($_POST['course_number'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE scu_equivalent_name='$equivalent'");
-		}
- 
-		else if(empty($_POST['university']) and empty($_POST['course_name'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
-		}
+        else if($university == $all and $course_name == $all and $equivalent == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_number='$course_number'");
+        }       
+                
+        else if($university == $all and $course_name == $all and $course_number == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE scu_equivalent_name='$equivalent'");
+        }
+        else if ($university == $all and $course_name == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
+        }
+        else if ($university == $all and $course_number == $all) { 
+                $result = mysqli_query($con,"SELECT * FROM courses WHERE course_name= '$course_name' AND scu_equivalent_name = '$equivalent'");
+        }
 
-		else if(empty($_POST['university']) and empty($_POST['course_number'])) { 
-			$result = mysqli_query($con,"SELECT * FROM courses WHERE course_name= '$course_name' AND scu_equivalent_name = '$equivalent'");
-		}
+        else if ($university == $all and $equivalent == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number'");
+        }
+        else if ($course_name == $all and $course_number == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND scu_equivalent_name = '$equivalent'");
+        }
+                
+        else if($course_name == $all and $equivalent == $all) {  
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number'");
+        }
 
-		else if(empty($_POST['university']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number'");
-		}
-
-		else if(empty($_POST['course_name']) and empty($_POST['course_number'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND scu_equivalent_name = '$equivalent'");
-		}
-
-		else if(empty($_POST['course_name']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number'");
-		}
-
-		else if(empty($_POST['course_number']) and empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_name = '$course_name'");
-		}
-
-		else if(empty($_POST['university'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
-		}
-
-		else if(empty($_POST['course_name'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
-		}
-
-		else if(empty($_POST['course_number'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND university = '$university' AND scu_equivalent_name = '$equivalent'");
-		}
-
-		else if(empty($_POST['scu_equivalent'])) { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number' AND university = '$university'");
-		}
-
-		else { 
-			$result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number' AND course_name = '$course_name' AND scu_equivalent_name = '$equivalent'"); 
-		}
-
-	}
-}
-else { 
-	$result = mysqli_query($con, "SELECT * FROM courses ORDER BY university"); 
-} 
-
-
-$row_cnt = $result->num_rows; 
-echo "<div style='margin-top: 8px'>"; 
-echo "Displaying " . $row_cnt . " results. ";
-echo "</div>"; 
- 
-
-echo "<div> 
-<table class='table table-striped'>
-<tr>
-<th>University</th>
-<th>Country</th>
-<th>City</th>
-<th>Course Name</th>
-<th>Course Number</th>
-<th>Equivalent Number </th>
-<th>Equivalent Name </th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-	echo "<tr>";
-	echo "<td>" . $row['university'] . "</td>";
-	echo "<td>" . $row['country'] . "</td>";
-	echo "<td>" . $row['city'] . "</td>";
-	echo "<td>" . $row['course_name'] . "</td>";
-	echo "<td>" . $row['course_number'] . "</td>";
-	echo "<td>" . $row['scu_equivalent'] . "</td>";
-	echo "<td>" . $row['scu_equivalent_name'] . "</td>"; 
-	echo "</tr>";
-} 
-echo "</table>
-</div>"; 
-
-$row_cnt = 0; 
-
-mysqli_close($con); 
-?>
+        else if ($course_number == $all and $equivalent == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_name = '$course_name'");
+        }
+                                                                                                                                                                                     
+        else if ($university == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
+        }
+                                                                                                                                                                                     
+        else if ($course_name == $all) { 
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number' AND scu_equivalent_name = '$equivalent'");
+        }
+	else if ($course_number == $all) {                                                                                                                                           
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND university = '$university' AND scu_equivalent_name = '$equivalent'");     
+        }                                                                                                                                                                            
+                                                                                                                                                                                     
+        else if ($equivalent == $all) {                                                                                                                                              
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE course_name = '$course_name' AND course_number = '$course_number' AND university = '$university'");        
+        }                                                                                                                                                                            
+                                                                                                                                                                                     
+        else {                                                                                                                                                                       
+                $result = mysqli_query($con, "SELECT * FROM courses WHERE university = '$university' AND course_number = '$course_number' AND course_name = '$course_name' AND scu_equivalent_name = '$
+equivalent'");
+        }                                                                                                                                                                            
+                                                                                                                                                                                     
+}                                                                                                                                                                                    
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+$row_cnt = $result->num_rows;                                                                                                                                                        
+echo "<div style='margin-top: 8px'>";                                                                                                                                                
+echo "Displaying " . $row_cnt . " results. ";                                                                                                                                        
+echo "</div>";                                                                                                                                                                       
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+echo "<div>                                                                                                                                                                          
+<table id='#table' class='table table-striped'>                                                                                                                                      
+<tr>                                                                                                                                                                                 
+<th>University</th>                                                                                                                                                                  
+<th>Country</th>                                                                                                                                                                     
+<th>City</th>                                                                                                                                                                        
+<th>Course Name</th>                                                                                                                                                                 
+<th>Course Number</th>                                                                                                                                                               
+<th>Equivalent Number </th>                                                                                                                                                          
+<th>Equivalent Name </th>                                                                                                                                                            
+</tr>";                                                                                                                                                                              
+                                                                                                                                                                                     
+while($row = mysqli_fetch_array($result))                                                                                                                                            
+{                                                                                                                                                                                    
+        echo "<tr>";                                                                                                                                                                 
+        echo "<td>" . $row['university'] . "</td>";                                                                                                                                  
+        echo "<td>" . $row['country'] . "</td>";                                                                                                                                     
+        echo "<td>" . $row['city'] . "</td>";                                                                                                                                        
+        echo "<td>" . $row['course_name'] . "</td>";                                                                                                                                 
+        echo "<td>" . $row['course_number'] . "</td>";                                                                                                                               
+        echo "<td>" . $row['scu_equivalent'] . "</td>";                                                                                                                              
+        echo "<td>" . $row['scu_equivalent_name'] . "</td>";                                                                                                                         
+        echo "</tr>";                                                                                                                                                                
+}                                                                                                                                                                                    
+echo "</table>                                                                                                                                                                       
+</div>";                                                                                                                                                                             
+                                                                                                                                                                                     
+$row_cnt = 0;                                                                                                                                                                        
+                                                                                                                                                                                     
+mysqli_close($con);                                                                                                                                                                  
+?>                                                                                                                                                                                   
+    
