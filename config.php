8f17bfb95f7d720951844c5c01ec0b7c36690f48
Christine Rohacz <crohacz@gmail.com>
2015-11-11 12:19:05 -0800

    Create config.php
    Config file used to include in others files. Need to set permissions so that this cannot be read by the user.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host_name = "dbserver.engr.scu.edu";
+$database = "sdb_crohacz";
+$username = "crohacz";
+$password = "00000896245";
+$con = mysqli_connect($host_name, $username, $password, $database);  
+
+if (mysqli_connect_errno()) {
+
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
