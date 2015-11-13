94f12d23da500d8ba2082476abf2206a46d68ecc
Christine Rohacz <crohacz@gmail.com>
2015-11-13 15:55:28 -0800

    Update loginbutton.php
    added session storage location

--- loginbutton.php
+++ loginbutton.php
@@ -1,4 +1,5 @@
 <?php
+session_save_path('/var/tmp');
 if(isset($_SESSION['login_user'])){
 	echo '<a href="logout.php"> <button id="login" type="button" class="btn btn-default navbar-right">Logout</button></a>';
 }else{
