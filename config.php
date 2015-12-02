bfc7a7f090e5e6e122542632d4b5f9ca44e726fb
Christine Rohacz <crohacz@gmail.com>
2015-12-01 18:03:21 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
+
 <?php
-$host_name = "dbserver.engr.scu.edu";
-$database = "sdb_crohacz";
+$host_name = "dbserver.engr.scu.edu"; 
+$database = "sdb_crohacz"; 
 $username = "crohacz";
 $password = "00000896245";
-$con = mysqli_connect($host_name, $username, $password, $database);  
+$con = mysqli_connect($host_name, $username, $password, $database); 
+ 
+if (mysqli_connect_errno()) { 
 
-if (mysqli_connect_errno()) {
-
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
 }
 
 ?>
